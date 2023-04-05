30c22dc6adf597e75ae7de66fa854ebc26118045
Scott Wales <Scott.Wales@bom.gov.au>
2023-04-05 14:01:55 +1000

    Base image

---
+++ etc/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Load Spack
+source $SPACK_ROOT/share/spack/setup-env.sh
+
+# Load environment
+spack activate $SPACK_ENV
---
+++ etc/spack-bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install Spack
+wget https://github.com/spack/spack/releases/download/v${SPACK_VERSION}/spack-${SPACK_VERSION}.tar.gz | tar -C $SPACK_ROOT -x
+
+# Activate Spack
+source $SPACK_ROOT/share/spack/setup-env.sh
+
+# Install the CI mirror and package repos
+spack mirror add --scope site ci-mirror file://$SPACK_CACHE
+spack repo add --scope site $SPACK_ROOT/var/spack/repos/jopa
+
+# Update Spack with system packages
+spack compiler find --scope site
+spack external find --scope site --all
+spack bootstrap now
+
+# List the found environment
+spack compilers
+spack find
