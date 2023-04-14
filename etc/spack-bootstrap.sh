673bea0df3aa5732410d55dd7933c3a48109f42b
Scott Wales <scott.wales@bom.gov.au>
2023-04-14 04:05:07 +0000

    Add jedi stack

--- etc/spack-bootstrap.sh
+++ etc/spack-bootstrap.sh
@@ -10,6 +10,10 @@ source $SPACK_ROOT/share/spack/setup-env.sh
 spack mirror add --scope site ci-mirror file://$SPACK_CACHE
 spack repo add --scope site $SPACK_ROOT/var/spack/repos/jopa
 
+# Install the JEDI spack repo
+JEDI_STACK=1.3.0
+wget -O - https://github.com/NOAA-EMC/spack/archive/refs/tags/spack-stack-${JEDI_STACK}.tar.gz | tar xz -C $SPACK_ROOT --strip-components=1 spack-spack-stack-${JEDI_STACK}/var/spack/repos/jcsda-emc spack-spack-stack-${JEDI_STACK}/var/spack/repos/jcsda-emc-bundles
+
 # Update Spack with system packages
 spack compiler find --scope site /usr
 spack external find --scope site --all --path /usr --not-buildable
