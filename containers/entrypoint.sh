9b2e8eb3c329b15cc9207e41352b7e9f50efdfb0
Scott Wales <scott.wales@bom.gov.au>
2023-05-16 09:48:50 +1000

    Disable isolation

--- containers/entrypoint.sh
+++ containers/entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [ -z "${SPACK_DONT_ISOLATE:-}" ]; then
+    # Don't consider the user's ~/.spack directory
+    export SPACK_DISABLE_LOCAL_CONFIG=1
+fi
+
+if [ -z "${PYTHON_DONT_ISOLATE:-}" ]; then
+    # Don't consider the user's ~/.local directory
+    export PYTHONNOUSERSITE=1
+    export PYTHONPATH=""
+fi
+
 export SPACK_PYTHON=$MAMBA_ROOT/envs/container/bin/python
 
 # Load Spack and Mamba
