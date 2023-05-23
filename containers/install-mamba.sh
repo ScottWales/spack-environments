961303d754584365530da3812749e19d742c23ef
Scott Wales <scott.wales@bom.gov.au>
2023-05-23 14:01:38 +1000

    Update mamba

--- containers/install-mamba.sh
+++ containers/install-mamba.sh
@@ -8,9 +8,15 @@ set -o pipefail
 cd $MAMBA_ROOT
 curl -L -O https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh
 bash Mambaforge-Linux-x86_64.sh -b -f -p $MAMBA_ROOT
+
+# Base environment
 $MAMBA_ROOT/bin/mamba install conda-lock
+$MAMBA_ROOT/bin/mamba update --all
+
+# Container environment
 $MAMBA_ROOT/bin/mamba create -n container python pyyaml
 $MAMBA_ROOT/bin/mamba clean --all
 $MAMBA_ROOT/bin/conda config --system --append channels "$MAMBA_REPO"
+
 rm Mambaforge-Linux-x86_64.sh
 
