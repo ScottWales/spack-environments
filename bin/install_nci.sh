ef004563e71a7e4ac49b62bea64bb00398b9e23d
Scott Wales <scott.wales@bom.gov.au>
2023-07-17 20:40:44 +1000

    Extract MPI changes from JOPA

--- bin/install_nci.sh
+++ bin/install_nci.sh
@@ -3,7 +3,7 @@
 #  Author Scott Wales
 #PBS -l wd
 #PBS -l ncpus=8
-#PBS -l mem=32gb
+#PBS -l mem=64gb
 #PBS -l jobfs=100gb
 #PBS -l walltime=1:00:00
 
@@ -16,12 +16,11 @@ INSTALL_ROOT=$PWD
 ENV=$1
 
 export SPACK_COMPILER=intel@2021.8.0
-export SPACK_MPI="openmpi@4.1.4 ~static"
+export SPACK_MPI=nci-openmpi@4.1.4
 
 # Activate spack
 export SPACK_SYSTEM_CONFIG_PATH=$INSTALL_ROOT/config/gadi
-#source /g/data/access/spack/0.19.0/share/spack/setup-env.sh
-source /home/562/saw562/projects/spack-pipeline/spack/share/spack/setup-env.sh
+source /scratch/hc46/saw562/spack-base/share/spack/setup-env.sh
 
 export SPACK_JOBS=${PBS_NCPUS:-4}
 
