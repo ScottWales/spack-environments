5b43534dfc7da74975daf6ba1895e1e9dd1e1468
Junwei Lyu <jl8309@gadi-login-01.gadi.nci.org.au>
2023-11-14 16:16:42 +1100

    Add lfric-tau-rose-stem Spack environment for lfric rose stem to work with TAU; Set TAU environment variables inside the container with the modified env.activate.sh

---
+++ envs/lfric-tau-v0/env.activate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#  Copyright 2023 Bureau of Meteorology
+#  Author Scott Wales
+
+: ${SPACK_ENV_VIEW:=$SPACK_ENV/.spack-env/view}
+
+export FCM_KEYWORDS=${FCM_KEYWORDS:-/build/fcm-keyword.cfg}
+export LDMPI=tau_f90.sh
+export FPP="cpp -traditional"
+export FFLAGS="${FFLAGS:-} -I$SPACK_ENV_VIEW/include -I$SPACK_ENV_VIEW/lib"
+export PFUNIT=$SPACK_ENV_VIEW
+export PSYCLONE_CONFIG=${PSYCLONE_CONFIG:-/build/psyclone.cfg}
+
+spack load tau
+export TAU_OPTIONS="-optKeepFiles -optCompInst -optMemDbg"
+export TAU_TRACK_MEMORY_LEAKS=1 
+export TAU_TRACK_IO_PARAMS=1
