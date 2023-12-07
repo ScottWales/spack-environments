f04870150357e60f0ab94a3e7f432f8da7d80140
Scott Wales <scott.wales@bom.gov.au>
2023-12-08 09:41:46 +1100

    Update fms

--- repos/bom-ngm/packages/fms/package.py
+++ repos/bom-ngm/packages/fms/package.py
@@ -2,6 +2,7 @@ class Fms(CMakePackage):
     git = "https://github.com/jcsda/FMS"
     url = "https://github.com/jcsda/FMS/archive/refs/tags/1.1.0.jcsda.tar.gz"
 
+    version("2023.03", sha256="008a9ff394efe6a8adbcf37dd45ca103e00ae25748fc2960b7bc54f2f3b08d85")
     version(
         "1.1.0.jcsda",
         sha256="47cccea0cf107a37b1a4978d82a862404f1280af54af93b258ddc9a7b865d011",
