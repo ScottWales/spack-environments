cd7852bbf19da53478c4a525c48cecf2bbc98eb5
Scott Wales <scott.wales@bom.gov.au>
2023-12-11 14:02:20 +1100

    Add fms to cpath

---
+++ envs/jopa-v0/post-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#  Copyright 2023 Bureau of Meteorology
+#  Author Scott Wales
+
+set -eu
+set -o pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "$(readlink -f ${BASH_SOURCE[0]})" )" &> /dev/null && pwd )
+
+# Add FMS
+echo "CPATH=$(spack find --format '{prefix}' fms)/include_r4:\$CPATH" >> $SPACK_ROOT/bin/activate-full.sh
