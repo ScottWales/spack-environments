1578b2354519ded89735147d1f140f5a1d20f17c
Junwei Lyu <junwei.lyu@bom.gov.au>
2024-02-14 14:28:12 +1100

    Tried to rebuild TAU and added back memory tracing options as default for TAU

--- envs/lfric-tau-v0/env.activate.sh
+++ envs/lfric-tau-v0/env.activate.sh
@@ -18,7 +18,10 @@ TAU_MAKEFILE=$(eval spack find --format='{prefix}' tau)
 TAU_MAKEFILE+='/lib/Makefile*'
 export TAU_MAKEFILE=$(eval ls $TAU_MAKEFILE)
 
-export TAU_OPTIONS="-optKeepFiles -optCompInst"
-#export TAU_TRACK_MEMORY_LEAKS=1 
-#export TAU_TRACK_IO_PARAMS=1
-#export TAU_TRACK_HEAP=1
+export TAU_OPTIONS="-optKeepFiles -optCompInst -optMemDbg"
+export TAU_TRACK_MEMORY_LEAKS=1 
+export TAU_TRACK_IO_PARAMS=1
+export TAU_TRACK_HEAP=1
+export TAU_COMM_MATRIX=1
+export TAU_CALLPATH=1 
+export TAU_CALLPATH_DEPTH=100
