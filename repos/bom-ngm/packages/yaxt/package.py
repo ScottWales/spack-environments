6087a88ac1749b84d08025ca6f3fc7399f060c74
davelee2804 <davelee2804@gmail.com>
2024-11-08 09:31:41 +1100

    adding the checksum for the yaxt 0.11.3 tarball, as determined via sha256sum on Joerg's advice

--- repos/bom-ngm/packages/yaxt/package.py
+++ repos/bom-ngm/packages/yaxt/package.py
@@ -10,7 +10,7 @@ class Yaxt(AutotoolsPackage):
 
     maintainers = ['scottwales']
 
-    version('0.11.3',url="https://swprojects.dkrz.de/redmine/attachments/download/541/yaxt-0.11.3.tar.gz")
+    version('0.11.3','1dde53de4805b3fb32256d61c9c18b937252345009ff82db3ad78c322181f5a7',url="https://swprojects.dkrz.de/redmine/attachments/download/541/yaxt-0.11.3.tar.gz")
     version('0.9.3.1','5cc2ffeedf1604f825f22867753b637d41507941b7a0fbbfa6ea40637a77605a',url="https://swprojects.dkrz.de/redmine/attachments/download/523/yaxt-0.9.3.1.tar.gz")
     version('0.9.0', 'd3673e88c1cba3b77e0821393b94b5952d8ed7dc494305c8cf93e7ebec19483c', url='https://swprojects.dkrz.de/redmine/attachments/download/498/yaxt-0.9.0.tar.gz')
 
